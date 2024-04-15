7c6a5607ddf25086483d1060078c71d9fec1536a
Traore Bernard <bernard_traore@ens.univ-artois.fr>
2024-04-15 11:34:21 +0200

    Complete ClientsController.ts tasks

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -21,4 +21,44 @@ export class ClientsController {
     PATCH /clients/:id
     DELETE /clients/:id
     */
+
+  @Get() // /clients?role
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return 'Welcome ' + role;
+  }
+
+  @Get('docs') // /clients/docs or /clients/docs?version=5
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): { id: string } {
+    return { id };
+  }
+
+  @Post()
+  createAlternative(@Body() user: {}) {
+    return user;
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  @HttpCode(204)
+  create(): string {
+    return 'Always';
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
 }
